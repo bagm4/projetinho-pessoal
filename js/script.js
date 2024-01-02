2a1e7ff6b15f907510ae16eebff817a033358413
bagm4 <110988499+bagm4@users.noreply.github.com>
2024-01-02 11:21:40 -0300

    rs

--- js/script.js
+++ js/script.js
@@ -1,53 +1,47 @@
-window.document.querySelector('.formContainer').addEventListener('submit', function(event){
-    event.preventDefault()
-    var hora1 = document.querySelector('#hora1')
-    var hora2 = document.querySelector('#hora2')
-    var containerResposta = document.querySelector('.containerResposta')
-    var mensagemError = document.querySelector('.mensagemError')
+window.document.querySelector('.formContainer').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var hora1 = document.querySelector('#hora1');
+    var hora2 = document.querySelector('#hora2');
+    var containerResposta = document.querySelector('.containerResposta');
+    var mensagemError = document.querySelector('.mensagemError');
 
-    var h1 = hora1.value
-    var h2 = hora2.value
-    
-    var valorh1 = null
-    var valorh2 = null
+    var h1 = parseFloat(hora1.value);
+    var h2 = parseFloat(hora2.value);
 
-    
-    validationValor()
+    var valorh1 = null;
+    var valorh2 = null;
 
-    function calculo1h(){ 
-        return valorh1 = ((50/h1).toFixed(2))  
+    validationValor();
+
+    function calculo1h() {
+        return valorh1 = (50 / h1).toFixed(2);
     }
 
-    function calculo2h(){
-        return valorh2 = (((50/h2) + (50/h1)).toFixed(2))
+    function calculo2h() {
+        return valorh2 = ((50 / h2) + (50 / h1)).toFixed(2);
     }
-    
-    function validationValor(){
-        if(h1 != "" && h2 != "null"){
-            calculo1h()
-            calculo2h()
-            hora1.value = null
-            hora2.value = null
-            
 
-            containerResposta.style.display = 'block'
+    function validationValor() {
+        if (!isNaN(h1) && !isNaN(h2) && h1 !== 0 && h2 !== 0) {
+            calculo1h();
+            calculo2h();
+            hora1.value = null;
+            hora2.value = null;
+
+            containerResposta.style.display = 'block';
 
             var respostaH1 = document.querySelector('.respostaH1');
             var respostaH2 = document.querySelector('.respostaH2');
 
-            respostaH1.textContent = `${valorh1}`;
-            respostaH2.textContent = `${valorh2}`;
+            respostaH1.textContent = `R$ ${valorh1}`;
+            respostaH2.textContent = `R$ ${valorh2}`;
+    
 
-            mensagemError.style.display = 'none'
-            return
+            mensagemError.style.display = 'none';
+            return;
+        } else {
+            mensagemError.style.display = 'block';
+            containerResposta.style.display = 'none';
         }
-        else{
-            mensagemError.style.display = 'block'
-            containerResposta.style.display = 'none'
-        }
-    } 
-
-})
-
-
-
+    }
+});
